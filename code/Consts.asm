050fbf391523afb18c359251d82afd6a32a73f92
HJoan <66239936+Joan0018@users.noreply.github.com>
2020-11-29 17:24:31 +0800

    Update Comment
    Update Comment

--- code/Consts.asm
+++ code/Consts.asm
@@ -3,17 +3,18 @@
 ;================================================================================================
 
 ;Main constants
-WindowWidth                 EQU     80
-WindowHeight                EQU     25
-ChatWindowHeight			EQU		28
-CurrentPage                 EQU     0
-MaxUserNameSize             EQU     16
-MaxMsgSize					EQU		200
-ChatBoxMargin               EQU     20
+WindowWidth                 EQU     80			;Fixed the overall program's window width
+WindowHeight                EQU     25			;Fixed the height of the window
+ChatWindowHeight			EQU		28			;Set for the height of the chat box
+CurrentPage                 EQU     0			;Set the current page of the window, for display purpose
+MaxUserNameSize             EQU     16			;The maximum length of the username in Main.asm
+MaxMsgSize					EQU		200			;The maximum length of the message in chat.asm
 
 ;Keys codes
-ESC_ScanCode                EQU     01H
-ESC_AsciiCode               EQU     1BH
+;ScanCode = For input Checking
+;AsciiCode = For compare purpose
+ESC_ScanCode                EQU     01H			
+ESC_AsciiCode               EQU     1BH			
 Enter_ScanCode              EQU     1CH
 Enter_AsciiCode             EQU     0DH
 Back_ScanCode               EQU     0EH
--- code/Graphics.asm
+++ code/Graphics.asm
@@ -2,29 +2,29 @@
 
 ;Set cursor position to (X, Y) in PageNum
 SetCursorPos MACRO X, Y, PageNum
-    MOV AH, 02H
-    MOV BH, PageNum
-    MOV DL, X
-    MOV DH, Y
-    INT 10H
+    MOV AH, 02H                 ;To Set the Cursor Position
+    MOV BH, PageNum             ;Set the current page number for display (page number 0...7)
+    MOV DH, Y                   ;Set the Position of Y-Coordinate (Row)
+    MOV DL, X                   ;Set the Position of X-Coordinate (Column)
+    INT 10H                     ;Interrupt
 ENDM SetCursorPos
 
-;Get cursor position in PageNum: DL=X, DH=Y
+;Get cursor position in PageNum: DH=Y, DL=X
 GetCursorPos MACRO PageNum
-    MOV AH, 03H
-    MOV BH, PageNum
-    INT 10H
+    MOV AH, 03H                 ;To Get the Cursor Position and size
+    MOV BH, PageNum             ;Set the current page number
+    INT 10H     
 ENDM GetCursorPos
 
 ;Scroll up or clear screen from (x1, y1) to (x2, y2)
 ScrollUp MACRO X1, Y1, X2, Y2, LinesCount
-    MOV AH, 06H
-    MOV AL, LinesCount
-    MOV BH, 07H
-    MOV CL, X1
-    MOV CH, Y1
-    MOV DL, X2
-    MOV DH, Y2
+    MOV AH, 06H                 ;Scroll up window (07H - scroll down window)
+    MOV AL, LinesCount          ;number of lines by which to scroll (00H = clear entire window)
+    MOV BH, 07H                 ;Attribute used to write blank lines at bottom of window
+    MOV CH, Y1                  ;Column of window's upper left corner
+    MOV CL, X1                  ;Row of window's upper left corner
+    MOV DH, Y2                  ;Row of window's upper left corner
+    MOV DL, X2                  ;Column of window's lower left corner
     INT 10H
 ENDM ScrollUp
 
@@ -42,7 +42,7 @@ ENDM ClearScreen
 ;Display a character number of times with a certain color
 PrintColoredChar MACRO Char, Color, Cnt, PageNum
     MOV AH, 09H         ;Display
-    MOV BH, PageNum     ;Page 0
+    MOV BH, PageNum     ;Set the current page number for display (page number 0...7)
     MOV AL, Char        ;Character to display
     MOV BL, Color       ;Color(back:fore)
     MOV CX, Cnt         ;Number of times
@@ -51,14 +51,15 @@ ENDM PrintColoredChar
 
 ;Draw pixel in (X, Y) position with certain color
 DrawPixel MACRO X, Y, Color
-    MOV AH, 0CH
-    MOV AL, Color
-    MOV CX, X
-    MOV DX, Y
+    MOV AH, 0CH         ;Change Color for a single pixel
+    MOV AL, Color       ;Pixel color
+    MOV CX, X           ;column
+    MOV DX, Y           ;row
     INT 10H
 ENDM DrawPixel
 
 ;Draw a vertical line starting from (X, Y) with a certain length and width
+;HorizontalWidth = number of times that wanted to display
 DrawLine MACRO StartX, StartY, VerticalLength, HorizontalWidth, Char, Color, PageNum
     LOCAL Back
     
